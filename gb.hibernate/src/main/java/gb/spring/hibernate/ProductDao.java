a5c3d0dbb28c4e255d661d1774aac4f7ed904a78
Egor_Khaziev <egor5.87@mail.ru>
2021-07-12 22:27:04 +0600

    черновик

---
+++ gb.hibernate/src/main/java/gb/spring/hibernate/ProductDao.java
@@ -0,0 +1,22 @@
+package gb.spring.hibernate;
+
+import java.util.List;
+
+public class ProductDao {
+
+    public Product findById(Long id) {
+        return null;
+    }
+
+    public List<Product> findAll() {
+        return null;
+    }
+
+    public void deleteById(Long id) {
+
+    }
+
+    public Product saveOrUpdate(Product product) {
+        return null;
+    }
+}
---
+++ gb.hibernate/src/main/java/gb/spring/hibernate/UserDao.java
@@ -0,0 +1,20 @@
+package gb.spring.hibernate;
+
+
+import java.util.List;
+
+public class UserDao {
+
+    public User findById(Long id) {
+        return null;
+    }
+
+    public List<User> findAll() {
+        return null;
+    }
+
+    public User save(User user) {
+        return null;
+    }
+
+}
