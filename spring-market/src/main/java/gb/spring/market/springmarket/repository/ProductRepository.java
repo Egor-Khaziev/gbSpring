2c467926a5e2b12a271038ee53012785e183a26c
Egor_Khaziev <egor5.87@mail.ru>
2021-07-22 00:55:51 +0600

    add findAllByCostmin/max/between

--- spring-market/src/main/java/gb/spring/market/springmarket/repository/ProductRepository.java
+++ spring-market/src/main/java/gb/spring/market/springmarket/repository/ProductRepository.java
@@ -5,7 +5,11 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+        List<Product> findAllByCostBetween(int max, int min);
+        List<Product> findAllByCostGreaterThanEqual(int min);
+        List<Product> findAllByCostLessThanEqual(int max);
 }
